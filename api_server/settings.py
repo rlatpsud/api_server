c59aff0d42985fa1ef1a38302a4aa7cab380f096
rlatpsud <66771585+rlatpsud@users.noreply.github.com>
2025-04-18 11:23:26 +0900

    Update settings.py

--- api_server/settings.py
+++ api_server/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = ')((l=wv($nsnsciqs&5tuaae9+o$d-5cqf%nj%w2jk^ex=o0=^'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ')((l=wv($nsnsciqs&5tuaae9+o$d-5cqf%nj%w2jk^ex=o0=^')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
